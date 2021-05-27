7b55000c5e42c9f3eab246c0b8b0d63920328499
krpk1900 <krpk1900@gmail.com>
2021-05-28 00:58:50 +0900

    [Add] ABC138_Dの実装途中

---
+++ ABC/ABC138/ABC138_D.rb
@@ -0,0 +1,22 @@
+N, Q = gets.chomp.split.map(&:to_i)
+a = []
+b = []
+v = []
+x = []
+(1..N-1).each do |i|
+  line = gets.chomp.split.map(&:to_i)
+  a << line[0]
+  b << line[1]
+end
+(1..Q).each do |i|
+  line = gets.chomp.split.map(&:to_i)
+  v << line[0]
+  x << line[1]
+end
+
+G = []
+(1..N).each do |i|
+  G[i] <<
+end
+
+# 実装途中
